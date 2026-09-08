c0233984c336c8efad7bd799c12266c385f0468b
Dev Pradhan <devpradhan444@gmail.com>
2026-09-08 15:03:32 +0530

    Add image manipulation features: enhance height, width, change background color, rotate, and move image

--- reactvite/src/component/Imagemanipulation.jsx
+++ reactvite/src/component/Imagemanipulation.jsx
@@ -8,6 +8,8 @@ function Imagemanipulation() {
     const[red,setRed]=useState(20);
     const[green,setGreen]=useState(200);
     const[blue,setBlue]=useState(140);
+    const[angle,setAngle]=useState(0);
+    const[marginLeft,setMarginLeft]=useState(0);
 
 function enhanceHeight(){
 setHeight(height+10);
@@ -17,15 +19,26 @@ setHeight(height+10);
 function enhanceWidth(){
 setWidth(width+10);
 }
-
+function changebgcolor(){
+  setRed(Math.floor(Math.random() * 256));
+  setGreen(Math.floor(Math.random() * 256));
+  setBlue(Math.floor(Math.random() * 256));
+}
+function rotateImage(){
+  setAngle((prevAngle) => prevAngle + 25);
+}
+function margin(){
+  setMarginLeft(marginLeft+10);
+}
   return (
     <div>
 <h2 style={{color:'white',backgroundColor:'brown'}}> Image Manipulation using React</h2>
-<div style={{border:'2px solid red',height:'300px', width:'400px',marginLeft:'300px'}}>
-<img src={cat} height={height} width={width} style={{backgroundColor:`rgb(${red},${green},${blue})`}}></img>
+<div style={{border:'2px solid red',height:'300px', width:'400px',marginLeft:`${marginLeft}px`}}>
+<img src={cat} height={height} width={width} style={{backgroundColor:`rgb(${red},${green},${blue})`,transform:`rotate(${angle}deg)`}}></img>
 <div>
     <h2>Cat height:{height}</h2>
     <h2>Cat width:{width}</h2>
+    <h2>Color code:({red}, {green}, {blue})</h2>
 </div>
 </div>
 <div>
@@ -33,6 +46,9 @@ setWidth(width+10);
 </div>
 <button onClick={enhanceHeight}>EnhanceHeight</button>
 <button onClick={enhanceWidth}>EnhanceWidth</button>
+<button onClick={changebgcolor}>Change Background Color</button>
+<button onClick={rotateImage}>Rotate Image</button>
+<button onClick={margin}>Move Image Left</button>
 
     </div>
   )
